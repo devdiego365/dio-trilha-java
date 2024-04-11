6f3aa6c575891566cfdb6c5918c12feb26c31587
devdiego365 <diegodasilva365@gmail.com>
2024-04-11 13:10:14 -0300

    Atualização do projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scan = new Scanner(System.in);
+       
+        System.out.println("Digite seu nome:");
+        String nome = scan.next();
+
+        System.out.println("Digite a sua agencia:");
+        int agencia = scan.nextInt();
+        
+        int saldo = 1000;
+        System.out.println("Seu saldo é: " + saldo);
+
+        System.out.println("Ola "+nome+" obrigado por criar uma conta em nosso banco sua agencia é "+agencia+ " e seu saldo é "+saldo
+                + "e ja esta disponivel para saque.");
+
     }
 }
